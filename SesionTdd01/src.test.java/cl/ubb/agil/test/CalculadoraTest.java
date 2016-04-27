253140e6dd83ad0e310b81c44f609ddeb890bb63
matias01 <mat16@live.cl>
2016-04-27 11:43:59 -0300

    primer test 2+3=5

--- SesionTdd01/src.test.java/cl/ubb/agil/test/CalculadoraTest.java
+++ SesionTdd01/src.test.java/cl/ubb/agil/test/CalculadoraTest.java
@@ -1,5 +1,13 @@
 package cl.ubb.agil.test;
 
+import org.junit.Test;
+
 public class CalculadoraTest {
+	@Test
+	public int dosMasTresEsCinco(){
+		Calculadora cal= new Calculadora();
+		int resultado= cal.sumar(2,3);
+		return(5);
+	}
 
 }
