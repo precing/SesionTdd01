f79d27cd5371b5928977804020830d3f9e633afb
matias01 <mat16@live.cl>
2016-04-27 12:08:00 -0300

    3º caso 200 menos 73 es 127 fail

--- SesionTdd01/src.test.java/cl/ubb/agil/test/CalculadoraTest.java
+++ SesionTdd01/src.test.java/cl/ubb/agil/test/CalculadoraTest.java
@@ -2,21 +2,33 @@ package cl.ubb.agil.test;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import cl.ubb.agil.Calculadora;
 
 public class CalculadoraTest {
+	private Calculadora cal;
+	@Before
+	public void setup(){
+		cal=new Calculadora();
+	}
+	
 	@Test
 	public void dosMasTresEsCinco(){
-		Calculadora cal= new Calculadora();
+		//Calculadora cal= new Calculadora();
 		//int resultado= cal.sumar(2,3);
 		assertEquals(5, cal.sumar(2,3));
 	}
 	@Test
 	public void menosUnoMasSiesteEsSeis(){
-		Calculadora cal=new Calculadora();
+		//Calculadora cal=new Calculadora();
 		assertEquals(6, cal.sumar(-1,7));
+	}
+	@Test
+	public void es200Menos73Es127(){
+		assertEquals(127, cal.restar(200.73));
+		
 	}
 
 }
