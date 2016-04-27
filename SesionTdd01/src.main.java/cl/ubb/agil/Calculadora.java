72811b6ef513c119f6fcbfba4b6c5c097db310e4
matias01 <mat16@live.cl>
2016-04-27 11:50:12 -0300

    primer test suma de 2 y 3 es igula 5, Exitoso

--- SesionTdd01/src.main.java/cl/ubb/agil/Calculadora.java
+++ SesionTdd01/src.main.java/cl/ubb/agil/Calculadora.java
@@ -1,5 +1,11 @@
 package cl.ubb.agil;
 
 public class Calculadora {
+	public Calculadora(){
+		
+	}
+	public int sumar(int numero1, int numero2){
+		return(5);
+	}
 
 }
--- SesionTdd01/src.test.java/cl/ubb/agil/test/CalculadoraTest.java
+++ SesionTdd01/src.test.java/cl/ubb/agil/test/CalculadoraTest.java
@@ -1,13 +1,17 @@
 package cl.ubb.agil.test;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
+
+import cl.ubb.agil.Calculadora;
 
 public class CalculadoraTest {
 	@Test
-	public int dosMasTresEsCinco(){
+	public void dosMasTresEsCinco(){
 		Calculadora cal= new Calculadora();
-		int resultado= cal.sumar(2,3);
-		return(5);
+		//int resultado= cal.sumar(2,3);
+		assertEquals(5, cal.sumar(2,3));
 	}
 
 }
